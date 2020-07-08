02af1795ed5ce1677c971c4fe78adb5ba3b83a42
Moshiur Rahman <rmoshiur705@gmail.com>
2020-07-08 12:58:22 +0600

    Binding custom properties & alias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-  h1{
-    color: red;
-  }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  serverElements = [
+    {type: 'server', name: 'Test', content: 'Test Content'},
+    {type: 'server', name: 'Test2', content: 'Test Content2'}
+  ];
 
 }
