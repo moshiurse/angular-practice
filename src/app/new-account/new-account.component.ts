a0802163f7236a60ec60f7c58635bc743d751b41
Moshiur Rahman <rmoshiur705@gmail.com>
2020-07-17 10:54:05 +0600

    Created Data sevice & injected for using in cross components

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,24 +1,26 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component} from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor(private loggingService: LoggingService){}
+  constructor(
+    private loggingService: LoggingService,
+    private accountsService: AccountsService
+    ){
+      this.accountsService.statusUpdated.subscribe((status: string) => {
+        alert('New Status : '+ status);
+      })
+    }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-
-    
-    this.loggingService.logStatus('A server status changed, new status: ' + accountStatus, accountStatus);
+    this.accountsService.addAccount(accountName, accountStatus);
+    // this.loggingService.logStatus('A server status changed, new status: ' + accountStatus, accountStatus);
   }
 }
