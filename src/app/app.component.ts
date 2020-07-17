fe20b74f238e2cb23d9fc9e8533c61c990fc2ae2
Moshiur Rahman <15215020+moshiurse@users.noreply.github.com>
2020-07-17 18:16:42 +0600

    initialized component for router setup (#10)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { AccountsService } from "./accounts.service";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-
-  accounts: {name: string, status: string}[] = [];
-
-  constructor(private accountsService: AccountsService){
-
-  }
-
-  ngOnInit(){
-    this.accounts = this.accountsService.accounts;
-  }
+export class AppComponent {
 }
