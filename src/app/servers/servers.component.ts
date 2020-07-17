d272f2eedbf077eb9f63685af97968fbaff9cc57
Moshiur Rahman <rmoshiur705@gmail.com>
2020-07-18 01:40:50 +0600

    Navigates routes programattically

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -10,10 +11,17 @@ export class ServersComponent implements OnInit {
 
   servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload(){
+    this.router.navigate(['/servers'], {relativeTo: this.route})
   }
 
 }
