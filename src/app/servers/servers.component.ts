35f3de482f063303ea628baeb87eed0100696738
Moshiur Rahman <rmoshiur705@gmail.com>
2020-07-18 00:21:26 +0600

    Navigating with routerLink & styling Active class

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,7 +7,7 @@ import { ServersService } from './servers.service';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  private servers: {id: number, name: string, status: string}[] = [];
+  servers: {id: number, name: string, status: string}[] = [];
 
   constructor(private serversService: ServersService) { }
 
