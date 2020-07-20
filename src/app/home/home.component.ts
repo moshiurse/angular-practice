ed34a3776d23654c779495719103d23a96a422b3
Moshiur Rahman <rmoshiur705@gmail.com>
2020-07-20 17:12:54 +0600

    Initialized For observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -9,23 +7,9 @@ import { AuthService } from '../auth.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  onLoadServer(id: number){
-    // navigate
-    this.router.navigate(['/servers', id, 'edit'],
-     {queryParams: {allowEdit: 1}, fragment: 'loading'})
-  }
-
-  onLogin(){
-    this.authService.login();
-  }
-
-  onLogout(){
-    this.authService.logout();
   }
 
 }
