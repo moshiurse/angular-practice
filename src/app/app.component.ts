246530d74460ff4201b8a470bdf95e3617d6292a
Moshiur Rahman <rmoshiur705@gmail.com>
2020-07-21 16:23:57 +0600

    added ngModel & show form property in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,17 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from "rxjs";
-import { UserService } from './user.service';
+import { Component } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-
-  userActvated: boolean = false;
-
-  private activatedSubject: Subscription;
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit() {
-    this.activatedSubject = this.userService.eventEmitter.subscribe((isActivated) => {
-      this.userActvated = isActivated;
-    })
+export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Superuser';
   }
 
-  ngOnDestroy(){
-    this.activatedSubject.unsubscribe();
+  onSubmit(form: NgForm){
+    console.log('Submitted', form);
   }
 }
